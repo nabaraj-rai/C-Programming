0a0e5991275d01f5081c5e966f21dc2e282a3f2a
Nabaraj Rai <115961746+nabaraj-rai@users.noreply.github.com>
2025-12-11 07:20:49 +0545

    if statements

---
+++ if-else.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter a number to be tested: ");
+    scanf("%d", &num);
+    if(num<0){
+        printf("The number %d is negative.",num);
+    }
+    return 0;
+}
