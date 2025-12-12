3d30cc22e1fae9bacd17bba7c3d7e25d68fe443b
Nabaraj Rai <115961746+nabaraj-rai@users.noreply.github.com>
2025-12-12 06:59:17 +0545

    Smallest among three numbers
    Nested If-else exercise: Smallest number among three numbers

---
+++ smallestAmongThree.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int n1, n2, n3;
+    printf("Enter three numbers: ");
+    scanf("%d%d%d",&n1,&n2,&n3);
+
+    if(n1<n2){
+        if(n1<n3){
+            printf("\n%d is the smallest number.", n1);
+        }
+        else{
+            printf("\n%d is the smallest number.",n3);
+        }
+    }
+    else{
+        if(n2<n3){
+            printf("\n%d is the smallest number.",n2);
+        }
+        else{
+            printf("\n%d is the smallest number.",n3);
+        }
+    }
+    return 0;
+}
