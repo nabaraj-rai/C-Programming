08f76367de8e7fef9a9180b68bfb829296ee4fd4
Nabaraj Rai <115961746+nabaraj-rai@users.noreply.github.com>
2025-12-12 07:09:31 +0545

    Largest among 3
    If - else if - else

---
+++ largestAmong3n.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n1, n2, n3;
+
+    printf("\nEnter 3 numbers: \t");
+    scanf("%d%d%d",&n1,&n2,&n3);
+    if(n1>n2&&n1>n3){
+        printf("\n%d is the largest number.",n1);
+    }
+    else if(n2>n1&&n2>n3){
+        printf("\n%d is the largest number.",n2);
+    }
+    else{
+        printf("\n%d is the largest number.",n3);
+    }
+    return 0;
+}
