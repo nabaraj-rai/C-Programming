94d9c51c0c403f0d379ead6f4cfbef26f41a4261
Nabaraj Rai <115961746+nabaraj-rai@users.noreply.github.com>
2025-12-31 07:04:26 +0545

    2D array Matrix I/O

---
+++ matrix2dArray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#define M 2
+#define N 4
+
+void main(){
+    int matrix[M][N],i,j;
+    printf("\nEnter the matrix elements:\t");
+    for(i=0;i<M;i++){
+        for(j=0;j<N;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    printf("\nThe entered matrix is:\n");
+    for(i=0;i<M;i++){
+        for(j=0;j<N;j++){
+            printf("%d\t",matrix[i][j]);
+        }
+        printf("\n");
+    }
+    getch();
+}
