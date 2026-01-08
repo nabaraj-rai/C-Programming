5ddb8c31ec2200184cbd30137c83e8bb34d1019e
Nabaraj Rai <115961746+nabaraj-rai@users.noreply.github.com>
2026-01-08 07:32:52 +0545

    greatest among 3 using function

--- f2.c
+++ f2.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-int greatest(int, int);
+int greatest(int, int, int);
 
 int main(){
-    int a, b, c;
+    int a, b, c,d;
     printf("Enter 1st number: \n");
     scanf("%d", &a);
     printf("Enter 2nd number: \n");
     scanf("%d", &b);
-    c=greatest(a,b);
-    printf("The greatest number is %d.",c);
+    printf("Enter 3rd number: \n");
+    scanf("%d", &c);
+    d=greatest(a,b,c);
+    printf("The greatest number is %d.",d);
     return 0;
 }
 
-int greatest(int a, int b){
-    if(a>b){
+int greatest(int a, int b, int c){
+    if(a>b&&a>c){
         return a;
     }
-    else{
+    else if(b>a&&b>c){
         return b;
+    }
+    else{
+        return c;
     }
 
 }
